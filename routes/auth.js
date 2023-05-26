d3ae50abf5ddf9443252b6f8e04a450d26af2358
Milad Mohammadi <phy.milad@gmail.com>
2023-05-26 13:46:04 +0330

    add login route

--- routes/auth.js
+++ routes/auth.js
@@ -24,4 +24,23 @@ router.post("/register", async (req, res) => {
   res.status(201).json({ user: newUser })
 })
 
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username })
+    if (!user) res.status(401).json("Wrong Password")
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASSWORD_SECRET_KEY
+    )
+    const pass = hashedPassword.toString(CryptoJS.enc.Utf8)
+    if (pass !== req.body.password) res.status(401).json("Wrong Credentials")
+
+    const { password, ...others } = user._doc
+    console.log(others)
+    res.status(200).json(others)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
 module.exports = router
