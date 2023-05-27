684b2987b559cf4779f80fd0bb61893794ac5dde
Milad Mohammadi <phy.milad@gmail.com>
2023-05-27 08:05:14 +0330

    define user routes and authentication middlewares

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,64 @@
 const router = require("express").Router()
+const CryptoJS = require("crypto-js")
+const User = require("../models/User")
+const {
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken")
+
+// Get all users
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find()
+    res.status(200).json(users)
+  } catch (err) {
+    res.status(500).json("There is a problem in server")
+  }
+})
+
+// Get user by id
+router.get("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    const { password, ...others } = user
+    res.status(200).json(...others)
+  } catch (err) {
+    res.status(500).json("cannot find this user")
+  }
+})
+
+// Update a user
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD_SECRET_KEY
+    ).toString()
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      {
+        new: true,
+      }
+    )
+    res.status(201).json(updatedUser)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// Delete a user
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id)
+    res.status(201).json("User has been deleted successfully")
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 module.exports = router
---
+++ routes/verifyToken.js
@@ -0,0 +1,47 @@
+const jwt = require("jsonwebtoken")
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token
+  // console.log(authHeader)
+  if (authHeader) {
+    jwt.verify(
+      authHeader.split(" ")[[1]],
+      process.env.JWT_SECRET_KEY,
+      (err, user) => {
+        if (err) res.status(403).json("token is not valid")
+        // console.log(user)
+        req.user = user
+        next()
+      }
+    )
+  } else {
+    return res.status(401).json("Auhentication failed")
+  }
+}
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    // console.log(req.user)
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next()
+    } else {
+      res.status(403).json("You are not allowed to do this")
+    }
+  })
+}
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if ((req, user.isAdmin)) {
+      next()
+    } else {
+      res.status(403).json("You are not allowed to do that")
+    }
+  })
+}
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+}
